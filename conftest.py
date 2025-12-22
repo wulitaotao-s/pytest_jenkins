96425ed28c2b787d8ad96afbfee460cfbd8e9be5
wutao <tao.wu@szjizhou.com.cn>
2025-12-22 19:15:24 +0800

    1915

--- conftest.py
+++ conftest.py
@@ -20,6 +20,7 @@ def driver():
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920,1080")
+    d = webdriver.Chrome(options=chrome_options)
     yield d
     d.quit()
 
