366f6c7da4c20861cb6fda3e8b2e08278300ae5a
wutao <wutao@szjiuzhou.com.cn>
2025-12-24 15:49:22 +0800

    login函数

--- conftest.py
+++ conftest.py
@@ -16,14 +16,14 @@ import requests
 
 @pytest.fixture(scope="function")
 def driver():
-    # chrome_options = Options()
-    # chrome_options.add_argument("--headless=new")
-    # chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--disable-dev-shm-usage")
-    # chrome_options.add_argument("--disable-gpu")
-    # chrome_options.add_argument("--window-size=1920,1080")
-    # d = webdriver.Chrome(options=chrome_options)
-    d = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+    d = webdriver.Chrome(options=chrome_options)
+    # d = webdriver.Chrome()
     yield d
     d.quit()
 
